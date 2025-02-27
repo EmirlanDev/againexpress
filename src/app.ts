e294fa3231df97013ac736f8c6676c6e59d8c085
EmirlanDev <frdevemirlan@gmail.com>
2025-02-27 19:28:52 +0600

    added get

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+config();
+import express from "express";
+
+export const buildServer = () => {
+  const server = express();
+  server.use(express.json());
+
+  server.get("/", (req, res) => {
+    res.status(200).send({
+      message: "Hello world",
+    });
+  });
+
+  return server;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello');
+import { buildServer } from "./app";
+
+const server = buildServer();
+
+const start = async () => {
+  const PORT = process.env.PORT || 3000;
+
+  try {
+    const httpHost = server.listen(
+      {
+        port: PORT,
+        host: "0.0.0.0",
+      },
+      () => {
+        console.log(`${new Date()}`);
+        console.log(`server run in: https://localhost:${PORT}`);
+      }
+    );
+  } catch (error) {
+    console.error(``);
+    process.exit(1);
+  }
+};
+
+start();
